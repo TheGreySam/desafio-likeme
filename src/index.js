42ce5bfcd96135cf30bf8d8fc2c5c2adea7d30f6
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2023-06-14 15:56:20 -0400

    desactivar metodo node
    react ya levanta el servidor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+/* import express from 'express';
+import path from 'path'
+//const express = require('express')
+//const path = require('path');
+
+const app = express();
+const PUERTO = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')) 
+});
+
+app.listen(PUERTO, () => {
+    console.log(`Server corriendo en puerto ${PUERTO}`)
+}) */
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
